2249ff639a80765203de0a98cd498a0dbd61a231
unknown <k.marciniak20@gmail.com>
2023-08-20 14:20:51 +0200

    stable

--- leds.ino
+++ leds.ino
@@ -1,9 +1,8 @@
 #include <LiquidCrystal_I2C.h>
-#include <Wire.h>
-#include <string.h> 
 
 using namespace std;
 LiquidCrystal_I2C lcd(0x27, 2, 1, 0, 4, 5, 6, 7, 3, POSITIVE);
+
 const int BUTTON1 = 13;
 const int BUTTON2 = 11;
 const int buzzer = 12;
@@ -12,18 +11,11 @@ const int LED2=3;
 const int LED3=4;
 const int LED4=5;
 int dt = 500;
+int led;
 
-int button1State = 0;
-int button1PushCounter = 0;
-int lastButton1State = 0;
-
-int button2State = 0;
-int button2PushCounter = 0;
-int lastButton2State = 0;
-
-int buttonsState = 0;
-
-
+char* selectedFunction;
+int selectedSpeed;
+int selectedCycles;
 
 void setup() {                
   pinMode(LED1, OUTPUT);  
@@ -40,147 +32,168 @@ void setup() {
 }
 
 void loop() {
+  int led;
+  int leds_size = 4;
+  int leds[leds_size] = {LED1, LED2, LED3, LED4};
 
-char* selectedFunction = -1;
-int selectedSpeed = -1;
-int selectedCycles = -1;
+  int options_size = 3;
+  char* options[options_size] = {"Functions", "Speed", "Cycles"};
 
-int leds[4] = {LED1, LED2, LED3, LED4};
+  int functions_size = 3;
+  char* functions[functions_size] = {"left", "right", "blink"};
 
-int options_size = 3;
-char* options[options_size] = {"Functions", "Speed", "Cycles"};
+  int speeds_size = 7;
+  int speeds[speeds_size] = {1000, 800, 600, 400, 200, 100, 50};
 
-int functions_size = 3;
-char* functions[functions_size] = {"left", "right", "blink"};
+  int cycles_size = 10;
+  int cycles[cycles_size] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
 
-int speeds_size = 5;
-int speeds[speeds_size] = {1000, 800, 600, 400, 200};
-
-int cycles_size = 5;
-int cycles[cycles_size] = {1, 2, 3, 4, 5};
-
-for ( char* option : options){
-
-  int actual = -1;
-  button1State = 0;
-  button1PushCounter = 0;
-  lastButton1State = 0;
-
-  button2State = 0;
-  lastButton2State = 0;
-
-  print(0, String(option));
-  
-  if (option == options[0]) {
-    while (selectedFunction == -1){
-      button1State = digitalRead(BUTTON1);
-      if (button1State != lastButton1State) {
-        if (button1State == HIGH) {
-          print(1, String(functions[button1PushCounter]));
-          button1PushCounter++;
-          actual = button1PushCounter;
-          if (button1PushCounter == functions_size){
-              button1PushCounter = 0;
-              }
-        } 
-        delay(50);
+  for ( char* option : options){
+    bool hasChanged = false;
+    int button1State = 0;
+    int button2State = 0;
+    int counter = 0;
+    int lastButton1State = 0;
+    int lastButton2State = 0;
+    print(0, String(option));    
+    // LEFT
+    if (option == options[0]) {
+      while (!hasChanged){
+        button1State = digitalRead(BUTTON1);
+        if (button1State != lastButton1State) {
+          if (button1State == HIGH) {
+            print(1, String(functions[counter]));
+            counter++;
+            selectedFunction = counter;
+            if (counter == functions_size){
+                counter = 0;
+            }
+          } 
+          delay(50);
+        }
+        button2State = digitalRead(BUTTON2);
+        if (button2State != lastButton2State) {
+          if (button2State != HIGH) {
+            hasChanged = true;
+          } 
+          delay(50);
+          lastButton2State = button2State;
+        }
+        lastButton1State = button1State;
       }
-
-      button2State = digitalRead(BUTTON2);
-      if (button2State != lastButton2State) {
-        if (button2State != HIGH) {
-          selectedFunction = actual;
-        } 
-        delay(50);
-        lastButton2State = button2State;
-      }
-      lastButton1State = button1State;
-    }
-
-  
+    // RIGHT
     } else if (option == options[1]) {
-    while (selectedSpeed == -1){
-      button1State = digitalRead(BUTTON1);
-      if (button1State != lastButton1State) {
-        if (button1State == HIGH) {
-          print(1, String(speeds[button1PushCounter]));
-          button1PushCounter++;
-          actual = button1PushCounter;
-          if (button1PushCounter == speeds_size){
-              button1PushCounter = 0;
-              }
-        } 
-        delay(50);
+      while (!hasChanged){
+        button1State = digitalRead(BUTTON1);
+        if (button1State != lastButton1State) {
+          if (button1State == HIGH) {
+            selectedSpeed = speeds[counter];
+            print(1, String(selectedSpeed));
+            counter++;
+            if (counter == speeds_size){
+                counter = 0;
+            }
+          } 
+          delay(50);
+        }
+        button2State = digitalRead(BUTTON2);
+        if (button2State != lastButton2State) {
+          if (button2State != HIGH) {
+            hasChanged = true;
+          } 
+          delay(50);
+          lastButton2State = button2State;
+        }
+        lastButton1State = button1State;
+      }    
+    // BLINK
+    } else if (option == options[2]) {
+      while (!hasChanged){
+        button1State = digitalRead(BUTTON1);
+        if (button1State != lastButton1State) {
+          if (button1State == HIGH) {
+            selectedCycles = cycles[counter];
+            print(1, String(selectedCycles));
+            counter++;
+            if (counter == cycles_size){
+                counter = 0;
+            }
+          } 
+          delay(50);
+        }
+        button2State = digitalRead(BUTTON2);
+        if (button2State != lastButton2State) {
+          if (button2State != HIGH) {
+            hasChanged = true;
+          } 
+          delay(50);
+          lastButton2State = button2State;
+        }
+        lastButton1State = button1State;
       }
-
-      button2State = digitalRead(BUTTON2);
-      if (button2State != lastButton2State) {
-        if (button2State != HIGH) {
-          selectedSpeed = speeds[actual];
-        } 
-        delay(50);
-        lastButton2State = button2State;
-      }
-      lastButton1State = button1State;
-    }
-
-  } else if (option == options[2]) {
-    while (selectedCycles == -1){
-      button1State = digitalRead(BUTTON1);
-      if (button1State != lastButton1State) {
-        if (button1State == HIGH) {
-          print(1, String(cycles[button1PushCounter]));
-          button1PushCounter++;
-          actual = button1PushCounter;
-          if (button1PushCounter == cycles_size){
-              button1PushCounter = 0;
-              }
-        } 
-        delay(50);
-      }
-
-      button2State = digitalRead(BUTTON2);
-      if (button2State != lastButton2State) {
-        if (button2State != HIGH) {
-          selectedCycles = actual;
-        } 
-        delay(50);
-        lastButton2State = button2State;
-      }
-      lastButton1State = button1State;
-    }
     }
   }
 
-delay(500);
-
-dt = selectedSpeed;
-for (int cycle = 0; cycle < selectedCycles; cycle++){
-  for (auto led : leds) {
-      digitalWrite(led, HIGH);
-      delay(dt);
-      }
-  tone(buzzer, 5000); // Send 1KHz sound signal...
-  delay(100);        // ...for 1 sec
   delay(dt);
-  noTone(buzzer);     // Stop sound...
-  delay(100);        // ...for 1sec
-
-  for (int led : leds) {
-      digitalWrite(led, LOW);
+  for (int cycle = 0; cycle < selectedCycles; cycle++){
+    // LEFT
+    if (selectedFunction == 1) {
+      dt = selectedSpeed / leds_size;
+      for (int i = 0; i < leds_size; i++){
+        ligh_on(leds[i]);
+        delay(dt);
+      }
+      ring();
+      for (int i = 0; i < leds_size; i++){
+        ligh_off(leds[i]);
+        delay(dt);
+      }
+    // RIGHT
+    } else if (selectedFunction == 2) {
+      dt = selectedSpeed / leds_size;
+      for (int i = leds_size; i > 0; i--){
+        ligh_on(leds[i-1]);
+        delay(dt);
+      }
+      ring();
+      for (int i = leds_size; i > 0; i--){
+        ligh_off(leds[i-1]);
+        delay(dt);
+      }
+    // BLINK
+    } else if (selectedFunction == 3) {
+      dt = selectedSpeed;
+      for (int i = leds_size; i > 0; i--){
+        ligh_on(leds[i-1]);
+      }
+      ring();
+      for (int i = leds_size; i > 0; i--){
+        ligh_off(leds[i-1]);
+      }
       delay(dt);
+    }
   }
 }
-
-
-
-}
-
 
 void print(int line, String text){
-      lcd.setCursor(0,line);
-      lcd.print("                ");
-      lcd.setCursor(0,line);
-      lcd.print(text);
+  lcd.setCursor(0,line);
+  lcd.print("                ");
+  lcd.setCursor(0,line);
+  lcd.print(text);
+}
+
+void ring(){
+  tone(buzzer, 5000);
+  delay(100);
+  delay(dt);
+  noTone(buzzer);
+  delay(100);
+}
+
+void ligh_on(int led){
+  digitalWrite(led, HIGH);
+}
+void ligh_off(int led){
+  digitalWrite(led, LOW);
 }
 
